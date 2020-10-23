131dea0478cf3b009a1725e3fb4c294158a4c6d2
pmarques93 <pmarques93@gmail.com>
2020-10-24 00:01:48 +0100

    Add movement. Add camera control

---
+++ Game/Assets/Scripts/PlayerLook.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLook : MonoBehaviour
+{
+    [Range(50, 250)][SerializeField] private float speed;
+    private Transform player;
+    private float verticalRotation;
+
+    void Start()
+    {
+        player = this.transform.parent.GetComponent<Transform>();
+        verticalRotation = 0f;
+
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        float horizontal    = Input.GetAxis("Mouse X") * speed * Time.deltaTime;
+        float vertical      = Input.GetAxis("Mouse Y") * speed * Time.deltaTime;
+
+        // Clamps vertical axis
+        verticalRotation -= vertical;
+        verticalRotation = Mathf.Clamp(verticalRotation, -70f, 70f);
+        // Rotates (vertical) around X axis
+        transform.localRotation = Quaternion.Euler(verticalRotation, 0f, 0f);
+
+        // Rotates the player on Y axis
+        player.Rotate(Vector3.up * horizontal);
+    }
+}
---
+++ Game/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Movement
+    [Range(1, 4)] [SerializeField] private float speed;
+    private Vector3 movement;
+    float zAxis, xAxis; 
+
+    // Components
+    private Rigidbody rb;
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        // Movement variable
+        movement = transform.right * xAxis + transform.forward * zAxis;
+        // Moves player
+        rb.velocity = movement.normalized * speed;
+    }
+
+
+    void Update()
+    {
+        // Gets movement axis
+        zAxis = Input.GetAxisRaw("Vertical");
+        xAxis = Input.GetAxisRaw("Horizontal");
+    }
+}
