f54938c74e2b0455ee0551e1ddb14cdc442c838e
pmarques93 <pmarques93@gmail.com>
2020-10-24 21:04:01 +0100

    Add WalkableRoom.cs WalkableRoom.cs creates bounds for a room automatically.

---
+++ Game/Assets/Scripts/WalkableRoom.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+sealed public class WalkableRoom : MonoBehaviour
+{
+    private BoxCollider roomCollider;
+
+    void Start()
+    {
+        roomCollider = GetComponent<BoxCollider>();
+
+        // Creates a bounds variable
+        Bounds roomBounds = new Bounds(transform.position, Vector3.zero);
+
+        // Creates bounds based on every renderer inside the parent object
+        foreach (Renderer obj in GetComponentsInChildren<Renderer>())
+        {
+            roomBounds.Encapsulate(obj.bounds);
+        }
+
+        // Sets the collider with the new measures
+        roomCollider.center = roomBounds.center;
+        roomCollider.size   = roomBounds.size;
+        roomCollider.isTrigger = true;
+    }
+}
