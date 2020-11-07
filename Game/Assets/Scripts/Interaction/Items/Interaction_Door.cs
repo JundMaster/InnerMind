6a8432819b674d95acf09d9acd7083fe1e871e00
mrverdinsky <goncalo.vila.verde@hotmail.com>
2020-11-07 22:10:44 +0000

    Add Player_Door Interaction + Door Open/Close Animation

---
+++ Game/Assets/Scripts/Interaction/Items/Interaction_Door.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interaction_Door : MonoBehaviour, IInteractable
+{
+    private Animator doorAnimation;
+    public void InteractionAction()
+    {
+        doorAnimation = GetComponent<Animator>();
+        doorAnimation.SetTrigger("openDoor");
+    }
+
+    public override string ToString() => "Open Door";
+}
