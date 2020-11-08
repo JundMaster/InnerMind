5d95a6f85c870f163f905f934078f363c5a02a5e
pmarques93 <pmarques93@gmail.com>
2020-11-08 20:23:13 +0000

    Fix door interaction

---
+++ Game/Assets/Scripts/Interaction/World/Interaction_Door.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interaction_Door : Interaction_Common
+{
+    public override void Execute()
+    {
+        Animator doorAnimation = GetComponent<Animator>(); ;
+        doorAnimation.SetTrigger("Open Door");
+    }
+
+    public override string ToString() => "Open Door";
+}
