f68b970f52c0ff73b2c7e23a08ba2c39944feac3
pmarques93 <pmarques93@gmail.com>
2020-11-26 22:50:19 +0000

    Add Neighbor model + Update room 3

--- Game/Assets/Scripts/PuzzleScripts/Room3Puzzle2/Room3.cs
+++ Game/Assets/Scripts/PuzzleScripts/Room3Puzzle2/Room3.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class Room3 : MonoBehaviour
 {
-    [SerializeField] private GameObject doorWithCode;
-    private InteractionDoorWithCode doorScript;
+    [SerializeField] private GameObject doorWithCodePuzzle2;
+    private InteractionDoorWithCode doorScriptPuzzle2;
+
+    [SerializeField] private GameObject doorPuzzle4;
+    [SerializeField] private ScriptableItem[] puzzle4PianoKeys;
 
     private PuzzlesEnum myPuzzle2, myPuzzle4;
 
@@ -14,7 +17,7 @@ public class Room3 : MonoBehaviour
     private void Awake()
     {
         player = FindObjectOfType<PlayerGeneralInfo>();
-        doorScript = doorWithCode.GetComponentInChildren<InteractionDoorWithCode>();
+        doorScriptPuzzle2 = doorWithCodePuzzle2.GetComponentInChildren<InteractionDoorWithCode>();
     }
 
     private void Start()
@@ -22,22 +25,29 @@ public class Room3 : MonoBehaviour
         myPuzzle2 = PuzzlesEnum.Puzzle2;
         myPuzzle4 = PuzzlesEnum.Puzzle4;
 
+        // Checks if player finished puzzle4
+        if (FindObjectOfType<Inventory>().Bag.Contains(puzzle4PianoKeys[0]) &&
+            FindObjectOfType<Inventory>().Bag.Contains(puzzle4PianoKeys[1]) &&
+            FindObjectOfType<Inventory>().Bag.Contains(puzzle4PianoKeys[2]))
+            Puzzle4Victory();
+
         if (player.PuzzlesDone.HasFlag(myPuzzle2))
-            doorWithCode.GetComponentInChildren<Animator>().SetTrigger("Open Door");
+            doorWithCodePuzzle2.GetComponentInChildren<Animator>().SetTrigger("Open Door");
 
         if (player.PuzzlesDone.HasFlag(myPuzzle4))
-        {    // CODE HERE //
+        {
+            doorPuzzle4.GetComponentInChildren<Animator>().SetTrigger("Open Door");
         }
     }
 
 
     private void OnEnable()
     {
-        doorScript.DoorOpened += Puzzle2Victory;
+        doorScriptPuzzle2.DoorOpened += Puzzle2Victory;
     }
     private void OnDisable()
     {
-        doorScript.DoorOpened -= Puzzle2Victory;
+        doorScriptPuzzle2.DoorOpened -= Puzzle2Victory;
     }
 
     private void Puzzle4Victory()
