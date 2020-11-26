9e94e2617a9e92f5602ca05613ba7b75ab79542f
mrverdinsky <goncalo.vila.verde@hotmail.com>
2020-11-26 20:17:20 +0000

    Add Puzzle Scripts to Room 1

---
+++ Game/Assets/Scripts/PuzzleScripts/Room1Puzzle1/Room1.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room1 : MonoBehaviour
+{
+    [SerializeField] private GameObject prize;
+    [SerializeField] private ScriptableItem prizeScriptableItem;
+    [SerializeField] private Transform prizePosition;
+
+    public bool FinishedPuzzle { get; private set; }
+
+    private PuzzlesEnum myPuzzle;
+    private void Start()
+    {
+        myPuzzle = PuzzlesEnum.Puzzle1;
+        FinishedPuzzle = false;
+
+        // If player has puzzle done and no map on inventory, plays victory
+        if (FindObjectOfType<PlayerGeneralInfo>().PuzzlesDone.HasFlag(myPuzzle) &&
+            FindObjectOfType<Inventory>().Bag.Contains(prizeScriptableItem) == false)
+        {
+            StartCoroutine(Victory());
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public IEnumerator Victory()
+    {
+        FindObjectOfType<PlayerGeneralInfo>().PuzzlesDone |= PuzzlesEnum.Puzzle2;
+        FinishedPuzzle = true;
+        GameObject spawn = null;
+
+        if (FindObjectOfType<Inventory>().Bag.Contains(
+            prizeScriptableItem) == false)
+        {
+            spawn = Instantiate(prize, prizePosition.transform.position, Quaternion.identity);
+        }
+
+        while (spawn)
+        {
+            spawn.transform.Rotate(15 * Time.deltaTime, 0, 15 * Time.deltaTime);
+            yield return null;
+        }
+
+        yield return null;
+    }
+}
