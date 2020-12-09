4ac447d10020e650ea1185cce590ff0da6b3741f
JundMaster <gabrielsantostroll@gmail.com>
2020-12-09 18:46:27 +0000

    Add documentation to Room6

--- Game/Assets/Scripts/PuzzleScripts/Room6Puzzle6/Room6.cs
+++ Game/Assets/Scripts/PuzzleScripts/Room6Puzzle6/Room6.cs
@@ -1,31 +1,44 @@
-﻿using System.Collections;
-using UnityEngine;
+﻿using UnityEngine;
 
+/// <summary>
+/// Responsible for controlling the puzzle of the room 6
+/// </summary>
 public class Room6 : PuzzleBase
 {
     // Prize related variables in inspector
     [SerializeField] private GameObject prize;
     [SerializeField] private ScriptableItem prizeScriptableItem;
     [SerializeField] private Animator drawerAnimator;
-    private bool isPrizePicked;
 
     private PictureFramePuzzleParent pictureFramePuzzleParent;
 
-    private void Bag_CollectionChanged(
+    #region Unity Functions
+    /// <summary>
+    /// Start method for Room6
+    /// </summary>
+    private void Start()
+    {
+        pictureFramePuzzleParent = FindObjectOfType<PictureFramePuzzleParent>();
+        pictureFramePuzzleParent.PuzzleSolved += Victory;
+        inventory.Bag.CollectionChanged += PrizedPicked;
+    }
+    #endregion
+
+    /// <summary>
+    /// Fires the animation for closing the drawer
+    /// </summary>
+    /// <param name="sender">The object that raised the event</param>
+    /// <param name="e">Information about the event</param>
+    private void PrizedPicked(
         object sender,
         System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
     {
         drawerAnimator.SetTrigger("CloseDrawer");
     }
 
-    private void Start()
-    {
-        pictureFramePuzzleParent = FindObjectOfType<PictureFramePuzzleParent>();
-        pictureFramePuzzleParent.PuzzleSolved += Victory;
-        inventory.Bag.CollectionChanged += Bag_CollectionChanged;
-        isPrizePicked = false;
-    }
-
+    /// <summary>
+    /// Does an action when the puzzle is solved
+    /// </summary>
     public override void Victory()
     {
         base.Victory();
