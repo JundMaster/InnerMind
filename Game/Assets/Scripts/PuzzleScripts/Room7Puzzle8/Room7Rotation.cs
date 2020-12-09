02507534b63dbc17f2fd0f0821dbb1de61692db3
pmarques93 <pmarques93@gmail.com>
2020-12-09 23:32:51 +0000

    Add initial room 7

---
+++ Game/Assets/Scripts/PuzzleScripts/Room7Puzzle8/Room7Rotation.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Class for Room7Rotation
+/// </summary>
+public class Room7Rotation : MonoBehaviour
+{
+
+    private Animator anim;
+
+    /// <summary>
+    /// Start method for Room7Rotation
+    /// </summary>
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    /// <summary>
+    /// OnTriggerEnter for Room7Rotation. Sets a trigger
+    /// </summary>
+    /// <param name="other"></param>
+    private void OnTriggerEnter(Collider other)
+    {
+        anim.SetTrigger("Stop Rotation");
+    }
+}
