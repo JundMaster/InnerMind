dbbdd82a19bcf943d3c6a05fa907edc9e3be5d0b
pmarques93 <pmarques93@gmail.com>
2020-12-16 21:33:39 +0000

    Remove spawns on room 5

--- Game/Assets/Scripts/PuzzleScripts/Room5Puzzle5/Room5.cs
+++ Game/Assets/Scripts/PuzzleScripts/Room5Puzzle5/Room5.cs
@@ -1,5 +1,5 @@
 ﻿using UnityEngine;
-using UnityEngine.UI;
+using System.Collections;
 
 /// <summary>
 /// Class responsible for Room5 puzzle. Extends PuzzleBase
@@ -16,6 +16,23 @@ public class Room5 : PuzzleBase
     [SerializeField] private Sprite mapImage;
     [SerializeField] private GameObject mapCanvas;
 
+    // Items to compare
+    [SerializeField] private ScriptableItem flaslight;
+    [SerializeField] private ScriptableItem noBatteryFlashlight;
+    [SerializeField] private ScriptableItem pianoKey2;
+
+    // Objects to destroy
+    [SerializeField] private GameObject flashlightSpawn;
+    [SerializeField] private GameObject pianoKeySpawn;
+
+    /// <summary>
+    /// Start method for Room5
+    /// </summary>
+    private void Start()
+    {
+        StartCoroutine(CheckItemSpawn());
+    }
+
     /// <summary>
     /// Update method of Room5
     /// </summary>
@@ -23,7 +40,7 @@ public class Room5 : PuzzleBase
     {
         // If the last wall animation hide wall is playing
         if (lastWallAnimator.GetCurrentAnimatorStateInfo(0).IsName("hideWall"))
-        {   
+        {
             // If the animation reached its end
             if (lastWallAnimator.GetCurrentAnimatorStateInfo(0).normalizedTime > 1)
             {
@@ -42,5 +59,18 @@ public class Room5 : PuzzleBase
 
         // After running base.Victory(), hides every walls
         movingWalls.SetActive(false);
+    }
+
+    private IEnumerator CheckItemSpawn()
+    {
+        yield return new WaitForSeconds(0.25f);
+
+        if (inventory.Bag.Contains(flaslight) ||
+            inventory.Bag.Contains(noBatteryFlashlight))
+            Destroy(flashlightSpawn);
+
+        if (inventory.Bag.Contains(pianoKey2) ||
+            player.PuzzlesDone.HasFlag(PuzzlesEnum.Puzzle3))
+            Destroy(pianoKeySpawn);
     }
 }
