5c1e428046e167e8d5f1e1fef37fe2ee5cfac8aa
pmarques93 <pmarques93@gmail.com>
2021-01-22 22:04:01 +0000

    Change inventory controls and mechanics. Inventory now opens with I key. Now the player needs to press the item with right click to show use and examine icons. Use icon will only show if the item is usable.

--- Game/Assets/Scripts/Player/IPlayerInput.cs
+++ Game/Assets/Scripts/Player/IPlayerInput.cs
@@ -1,67 +1,72 @@
 ﻿using UnityEngine;
 
 /// <summary>
-/// Interface for player inputs
+/// Interface for player inputs.
 /// </summary>
 public interface IPlayerInput
 {
     /// <summary>
-    /// Property for player current control
+    /// Property for player current control.
     /// </summary>
     TypeOfControl CurrentControl { get;}
 
     /// <summary>
-    /// Property for ZAxis
+    /// Property for ZAxis.
     /// </summary>
     float ZAxis { get; }
 
     /// <summary>
-    /// Property for XAxis
+    /// Property for XAxis.
     /// </summary>
     float XAxis { get; }
 
     /// <summary>
-    /// Propert for horizontal mouse speed
+    /// Propert for horizontal mouse speed.
     /// </summary>
     float HorizontalMouse { get; }
 
     /// <summary>
-    /// Propert for vertical mouse speed
+    /// Propert for vertical mouse speed.
     /// </summary>
     float VerticalMouse { get; }
 
     /// <summary>
-    /// Propert for left click
+    /// Propert for left click.
     /// </summary>
     bool LeftClick { get; }
 
     /// <summary>
-    /// Property for right click
+    /// Propert for left click.
     /// </summary>
     bool RightClick { get; }
 
     /// <summary>
-    /// Property for pause input
+    /// Property for right click.
+    /// </summary>
+    bool Inventory { get; }
+
+    /// <summary>
+    /// Property for pause input.
     /// </summary>
     bool Pause { get; }
 
     /// <summary>
-    /// Property to get key to pass a scene
+    /// Property to get key to pass a scene.
     /// </summary>
     bool Space { get; }
 
     /// <summary>
-    /// Property for cursor position relative to cursor in game
+    /// Property for cursor position relative to cursor in game.
     /// </summary>
     Vector2Int CursorPosition { get; }
 
     /// <summary>
-    /// Property for mouse speed
+    /// Property for mouse speed.
     /// </summary>
     float MouseSpeed { get; }
 
     /// <summary>
-    /// Method to change type of control
+    /// Method to change type of control.
     /// </summary>
     /// <param name="control">TypeOfControl to change to</param>
     void ChangeTypeOfControl(TypeOfControl control);
